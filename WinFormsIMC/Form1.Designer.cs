aa5c79d44bf7bbb89379d0768ac69394210f9704
TIAGO SILVA <frost.tiago@gmail.com>
2021-06-08 15:57:40 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsIMC/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WinFormsIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peso:";
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(13, 32);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(439, 23);
+            this.txt_peso.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Altura:";
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(13, 81);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(439, 23);
+            this.txt_altura.TabIndex = 3;
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_resultado.ForeColor = System.Drawing.Color.Red;
+            this.lbl_resultado.Location = new System.Drawing.Point(148, 177);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(166, 65);
+            this.lbl_resultado.TabIndex = 4;
+            this.lbl_resultado.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cálculo do IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinFormsIMC/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double peso = Convert.ToDouble(txt_peso.Text);
+            double altura = Convert.ToDouble(txt_altura.Text);
+
+            double imc = peso / (altura * altura);
+
+            lbl_resultado.Text = "Seu IMC é " + imc.ToString();
+        }
+    }
+}
